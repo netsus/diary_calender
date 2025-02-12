ecd2a9c92f5843fdc3b490adb10dcd34608cac82
wonjun <cwjcsk@naver.com>
2025-02-12 14:41:02 +0900

    [Feat] add Event

--- src/CalendarComponent.jsx
+++ src/CalendarComponent.jsx
@@ -8,8 +8,7 @@ const CalendarComponent = () => {
             plugins={[dayGridPlugin]}
             initialView="dayGridMonth"
             events={[
-                { title: 'Event 1', date: '2024-06-01' },
-                { title: 'Event 2', date: '2024-06-07' }
+                { title: 'Event 1', date: '2025-02-13' },
             ]}
         />
     );
